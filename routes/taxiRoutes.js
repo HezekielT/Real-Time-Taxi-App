2a9c2c098c25f0f71882e7a27713fa958b32edde
hizkielT <hezekiel.feyissa@gmail.com>
2022-03-05 10:59:01 +0300

    finished the route part

--- routes/taxiRoutes.js
+++ routes/taxiRoutes.js
@@ -1,16 +1,29 @@
 const express = require('express');
-
+const dbo = require("../server/db/conn");
 const driverRouter = express.Router();
 
-driverRouter.route('/').get(function(req, res){
-    //Ask permission for user's location as 
-});
+// driverRouter.route('/').get(function(req, res){
+//     //Ask permission for user's location as 
 
-driverRouter.route('/register').post();
+// });
 
-driverRouter.route('/login').get();
+// driverRouter.route('/register').post();
 
-driverRouter.route('/Home').get();
+driverRouter.route('/login').get(function(req, res){
+    
+let db_connect = dbo.getDb("myFirstDatabase");
+    db_connect
+    .collection("records")
+    .find({})
+    .toArray(function (err, result){
+        if (err) throw err;
+        res.json(result);
+    });
+}
+);
 
-driverRouter.route('/Home/Edit-Profile').get();
+// driverRouter.route('/Home').get();
 
+// driverRouter.route('/Home/Edit-Profile').get();
+
+module.exports = driverRouter;
